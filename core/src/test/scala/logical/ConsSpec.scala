dd0c967e67ed709cbf717674d166d5ea451f1538
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-08-30 18:55:31 +0900

    fix tests

--- core/src/test/scala/logical/ConsSpec.scala
+++ core/src/test/scala/logical/ConsSpec.scala
@@ -9,32 +9,32 @@ class ConsSpec extends FunSpec {
     it("append") {
       val xs = Var[Cons[Int]]
       val ys = Var[Cons[Int]]
-      assert((Cons.append(Cons(1, 2, 3), Cons(4, 5), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2, 3, 4, 5)))
-      assert((Cons.append(Cons(1, 2, 3), xs, Cons(1, 2, 3, 4, 5)) &&& xs.get.flatMap(_.toList)).run == Stream(List(4, 5)))
-      assert((Cons.append(xs, ys, Cons(1, 2, 3)) &&& (for (xs <- xs.get.flatMap(_.toList); ys <- ys.get.flatMap(_.toList)) yield (xs, ys))).run == Stream((Nil, List(1, 2, 3)), (List(1), List(2, 3)), (List(1, 2), List(3)), (List(1, 2, 3), Nil)))
+      assert((Cons.append(Cons(List(1, 2, 3)), Cons(List(4, 5)), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2, 3, 4, 5)))
+      assert((Cons.append(Cons(List(1, 2, 3)), xs, Cons(List(1, 2, 3, 4, 5))) &&& xs.get.flatMap(_.toList)).run == Stream(List(4, 5)))
+      assert((Cons.append(xs, ys, Cons(List(1, 2, 3))) &&& (for (xs <- xs.get.flatMap(_.toList); ys <- ys.get.flatMap(_.toList)) yield (xs, ys))).run == Stream((Nil, List(1, 2, 3)), (List(1), List(2, 3)), (List(1, 2), List(3)), (List(1, 2, 3), Nil)))
     }
 
     it("select") {
       val xs = Var[Cons[Int]]
-      assert((Cons.select(1, Cons(1, 2, 3), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(2, 3)))
-      assert((Cons.select(2, Cons(1, 2, 3), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 3)))
-      assert((Cons.select(2, Cons(2, 1, 2, 3), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2, 3), List(2, 1, 3)))
+      assert((Cons.select(1, Cons(List(1, 2, 3)), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(2, 3)))
+      assert((Cons.select(2, Cons(List(1, 2, 3)), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 3)))
+      assert((Cons.select(2, Cons(List(2, 1, 2, 3)), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2, 3), List(2, 1, 3)))
     }
 
     it("permutations") {
       val xs = Var[Cons[Int]]
-      assert((Cons.permutations(Cons(1, 2, 3), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2, 3), List(1, 3, 2), List(2, 1, 3), List(2, 3, 1), List(3, 1, 2), List(3, 2, 1)))
+      assert((Cons.permutations(Cons(List(1, 2, 3)), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2, 3), List(1, 3, 2), List(2, 1, 3), List(2, 3, 1), List(3, 1, 2), List(3, 2, 1)))
     }
 
     it("combinations") {
       val xs = Var[Cons[Int]]
-      assert((Cons.combinations(Nat(2), Cons(1, 2, 3), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2), List(1, 3), List(2, 3)))
+      assert((Cons.combinations(Nat(2), Cons(List(1, 2, 3)), xs) &&& xs.get.flatMap(_.toList)).run == Stream(List(1, 2), List(1, 3), List(2, 3)))
     }
 
     it("contains") {
       val xs = Var[Cons[Int]]
-      assert(Cons.contains(1, Cons(1, 2, 3)).run == Stream(()))
-      assert(Cons.contains(0, Cons(1, 2, 3)).run == Stream())
+      assert(Cons.contains(1, Cons(List(1, 2, 3))).run == Stream(()))
+      assert(Cons.contains(0, Cons(List(1, 2, 3))).run == Stream())
     }
 
   }
