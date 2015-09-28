8a5ac2194fdfa4785c2d762e3cb00136bdbfde8a
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-09-28 10:53:40 +0900

    add Fib example

--- core/src/main/scala/logical/Logic.scala
+++ core/src/main/scala/logical/Logic.scala
@@ -4,27 +4,15 @@ import scala.util.control.TailCalls._
 
 sealed trait Logic[+A] { self =>
 
-  final def run: Stream[A] = run(Env.empty).result
+  final def run: Stream[A] = run(Env.empty)
 
-  final def run(env: Env): TailRec[Stream[A]] =
-    this match {
-      case Failure => done(Stream.empty)
-      case Get(f) => done(Stream(f(env)))
-      case Put(env, value) => done(Stream(value))
-      case Or(self, f) => 
-        for {
-          x <- tailcall(self.run(env))
-          y <- tailcall(f().run(env))
-        } yield x.append(y)
-      case FlatMap(self, f) =>
-        self match {
-          case Failure => done(Stream.empty)
-          case Get(g) => tailcall(f(g(env)).run(env))
-          case Put(env, value) => tailcall(f(value).run(env))
-          case Or(self, g) => tailcall((self.flatMap(f) ||| g().flatMap(f)).run(env))
-          case FlatMap(self, g) => tailcall(self.flatMap(g(_).flatMap(f)).run(env))
-        }
+  final def run[B >: A](env: Env): Stream[B] = {
+    val (e, result) = Logic.resume(this, env, Stream.empty)
+    result match {
+      case Some((value, logs)) => value #:: logs.flatMap(_.run(e))
+      case None => Stream.empty
     }
+  }
 
   def map[B](f: A => B): Logic[B] = flatMap(a => Logic.success(f(a)))
 
@@ -38,25 +26,50 @@ sealed trait Logic[+A] { self =>
 
   def |||[B >: A](that: => Logic[B]): Logic[B] =
     this match {
-      case Or(self, f) => Or(self, () => Or(f(), () => that))
-      case _ => Or(this, () => that)
+      case Append(self, f) => Append(self, () => Append(f(), () => that))
+      case _ => Append(this, () => that)
     }
 
 }
 
-case class FlatMap[A, B](self: Logic[A], f: A => Logic[B]) extends Logic[B]
+case object Failure extends Logic[Nothing]
 
 case class Get[A](apply: Env => A) extends Logic[A]
 
 case class Put[A](env: Env, value: A) extends Logic[A]
 
-case class Or[A](self: Logic[A], f: () => Logic[A]) extends Logic[A]
+case class FlatMap[A, B](self: Logic[A], f: A => Logic[B]) extends Logic[B]
+
+case class Append[A](self: Logic[A], f: () => Logic[A]) extends Logic[A]
 
 case class Cut[A](self: Logic[A]) extends Logic[A]
 
-case object Failure extends Logic[Nothing]
-
 object Logic {
+
+  @annotation.tailrec
+  final def resume[A](log: Logic[A], env: Env, acc: Stream[Logic[A]]): (Env, Option[(A, Stream[Logic[A]])]) =
+    log match {
+      case Failure =>
+        if (acc.isEmpty)
+          (env, None)
+        else
+          resume(acc.head, env, acc.tail)
+      case Get(g) => (env, Some((g(env), acc)))
+      case Put(env, value) => (env, Some((value, acc)))
+      case Append(self, f) =>
+        self match {
+          case Failure => resume(f(), env, acc)
+          case _ => resume(self, env, f() #:: acc)
+        }
+      case FlatMap(self, f) =>
+        self match {
+          case Failure => resume(Failure, env, acc)
+          case Get(g) => resume(f(g(env)), env, acc)
+          case Put(env, value) => resume(f(value), env, acc)
+          case Append(self, g) => resume(self.flatMap(f), env, g().flatMap(f) #:: acc)
+          case FlatMap(self, g) => resume(self.flatMap(g(_).flatMap(f)), env, acc)
+        }
+    }
 
   val get: Logic[Env] = Get(state => state)
 
